e4b64c4169020ac5f685f1258395df013ff249d8
Schaene Trible <schaene@gmx.com>
2024-06-16 23:16:17 -0500

    Modified
    Unfinished, but modified to work again and have some added functionality

--- Chrome/d2lgrades.js
+++ Chrome/d2lgrades.js
@@ -1,49 +1,92 @@
-//Get "Grades" header and weighted grades
-var grades_heading=document.getElementsByClassName("vui-heading-1")[0];
-var grade_rows=document.getElementsByTagName("tr");
-var grade_cols_len=grade_rows[0].childElementCount;
-var grade_sums=[0, 0];  //[Grade, Weight]
+// Get "Grades" header and weighted grades
+var grades_heading = document.getElementsByClassName("vui-heading-1")[0];
+var grade_rows = document.getElementsByTagName("tr");
+var grade_cols_len = grade_rows[0].childElementCount;
+var grade_sums = [0, 0, 0, 0];  //[Grade, TotalGrade, Weight, TotalWeight]
 
-//Make list of grades/weights
+// Make list of grades/weights
 var curnode;
 var curgrade;
 
-for(var i=1; i<grade_rows.length;++i){
-	if(grade_rows[i].childElementCount==grade_cols_len){
-		curnode=grade_rows[i].childNodes[2];
-		curgrade=curnode.getElementsByTagName("label")[0].textContent.split('/');
-		//Add values to sums
-		console.log("curgrade: "+curgrade)
-		if(curgrade[0]!='- ' && curgrade[1]!=' -'){
-			grade_sums[0]+=parseFloat(curgrade[0]);
-			grade_sums[1]+=parseFloat(curgrade[1]);
-			console.log(grade_sums);
-		}//if()
-	}//if()
-}//for()
+// Find which column contains the "points" segment
+let grade_header = grade_rows[0].getElementsByTagName("th");
+let pointsIndex;
+for (let i = 0; i < grade_header.length; i++) {
+    console.log(i + grade_header[i].textContent)
+    if (grade_header[i].textContent === "Points") {
+        pointsIndex = i;
+    }
+}
+console.log(pointsIndex == null ? "Points index not found, assuming 2" : "Points index found, " + pointsIndex);
+pointsIndex = pointsIndex == null ? 2 : pointsIndex;
 
-grade_sums[0]=grade_sums[0].toFixed(2)
-var total_grade=((grade_sums[0]/grade_sums[1])*100);
+for (var i = 1; i < grade_rows.length; ++i) {
+    if (grade_rows[i].childElementCount == grade_cols_len) {
+        curnode = grade_rows[i].childNodes[pointsIndex];
+        curgrade = curnode.getElementsByTagName("label")[0].textContent.split('/');
+        // Add values to sums
+        console.log("curgrade: " + curgrade)
+        // If neither number is -, add to both grade and total grade
+        if (curgrade[0] != '- ' && curgrade[1] != ' -') {
+            grade_sums[0] += parseFloat(curgrade[0]);
+            grade_sums[1] += parseFloat(curgrade[0]);
+            grade_sums[2] += parseFloat(curgrade[1]);
+            grade_sums[3] += parseFloat(curgrade[1]);
 
-//Modify header
-grades_heading.innerHTML="Grades (Total: "+total_grade+"%)";
+        } // if()
+        // If first number is -, and last number is valid, treat as a 0 for total grade but ignore for grade
+        else if (curgrade[0] == '- ' && curgrade[1] != ' -') {
+            //grade_sums[0]+=parseFloat(curgrade[0]);
+            //grade_sums[0]+=parseFloat(curgrade[0]);
+            //grade_sums[1]+=parseFloat(curgrade[2]);
+            grade_sums[2] += parseFloat(curgrade[1]);
+        }
+        // If second number is null, assume bonus assignment
+        else if (curgrade[0] != '- ' && curgrade[1] == null) {
+            grade_sums[0] += parseFloat(curgrade[0]);
+            grade_sums[2] += parseFloat(curgrade[0]);
+        }
+        console.log(grade_sums);
+    } // if()
+} // for()
 
-//Add new table row below row 1
-var newrow=document.createElement("tr");
-var datanode=document.createElement("td");
-var tempspaces=['Total Grade', grade_sums[1]+'/100', grade_sums[0]+' / '+grade_sums[1], total_grade+'%'];
-var curclone;
-datanode.className="d_gn d_gr d_gt";
-for(var i=0;i<grade_cols_len;++i){
-	console.log("NEW COL")
-	curclone=datanode.cloneNode();
-	curclone.textContent=tempspaces[i];
-	newrow.appendChild(curclone);
-}//for()
-newrow.firstChild.style="text-align:left";
-if(document.getElementsByClassName("d_g_treeNodeImage").length>0){
-	newrow.insertBefore(document.createElement("th"), newrow.firstChild);
-}//if()
+grade_sums[0] = grade_sums[0].toFixed(2);
+var total_grade = ((grade_sums[0] / grade_sums[2]) * 100);
+var graded_grade = ((grade_sums[1] / grade_sums[3]) * 100);
+
+// Modify header
+//grades_heading.innerHTML = "Grades \n(Total: " + total_grade.toFixed(2) + "%, Graded: " + graded_grade.toFixed(2) + "%)";
+
+// Add new table rows below row 1
+var newTotalRow = document.createElement("tr");
+var newGradedRow = document.createElement("tr");
+var dataNode = document.createElement("td");
+
+// Total Grade Row
+var totalSpaces = ['Total Grade', grade_sums[0] + " / " + grade_sums[2], total_grade.toFixed(2) + '%'];
+for (var i = 0; i < grade_cols_len; ++i) {
+    var curClone = dataNode.cloneNode();
+    curClone.textContent = totalSpaces[i];
+    newTotalRow.appendChild(curClone);
+}
+newTotalRow.firstChild.style = "text-align:left";
+if (document.getElementsByClassName("d_g_treeNodeImage").length > 0) {
+    newTotalRow.insertBefore(document.createElement("th"), newTotalRow.firstChild);
+}
+
+// Graded Grade Row
+var gradedSpaces = ['Graded Grade', grade_sums[1].toFixed(2) + " / " + grade_sums[3], graded_grade.toFixed(2) + '%'];
+for (var i = 0; i < grade_cols_len; ++i) {
+    var curClone = dataNode.cloneNode();
+    curClone.textContent = gradedSpaces[i];
+    newGradedRow.appendChild(curClone);
+}
+newGradedRow.firstChild.style = "text-align:left";
+if (document.getElementsByClassName("d_g_treeNodeImage").length > 0) {
+    newGradedRow.insertBefore(document.createElement("th"), newGradedRow.firstChild);
+}
+
 console.log(grade_cols_len);
 
-grade_rows[0].after(newrow);
+grade_rows[0].after(newTotalRow);
+newTotalRow.after(newGradedRow);
